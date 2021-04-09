1d255db093ffebf7315d80f92b6765fa6b546d36
Adam Marshall <adam@appsynergy.net>
2021-04-09 21:47:42 +0100

    element prop wip

--- src/game/rnjesus.rs
+++ src/game/rnjesus.rs
@@ -3,13 +3,21 @@ use rand::prelude::*;
 
 use super::tiles::Element;
 
+pub fn d20() -> u8 {
+    rand_u8(1, 20)
+}
+
+pub fn d10() -> u8 {
+    rand_u8(1, 10)
+}
+
 pub fn rand_u8(x: u8, y: u8) -> u8 {
     let mut rng = rand::thread_rng();
-    rng.gen_range(x, y) as u8
+    rng.gen_range(x, y + 1) as u8
 }
 
 pub fn rand_element() -> Element {
     let mut rng = rand::thread_rng();
-    let index = rng.gen_range(0, 3);
+    let index = rng.gen_range(0, 4);
     [Element::Air, Element::Earth, Element::Fire, Element::Water][index]
 }
--- src/game/tiles/tile_elements.rs
+++ src/game/tiles/tile_elements.rs
@@ -60,13 +60,88 @@ impl TileElements {
     }
 
     pub fn propagate(tile_elements: &TileElements) -> Self {
-        let elements: ThreeElements = [
-            if rnjesus::rand_u8(0, 2) == 0 { rnjesus::rand_element() } else { tile_elements.elements[0] },
-            rnjesus::rand_element(),
-            rnjesus::rand_element(),
-        ];
-        TileElements::new(elements)
+        let elements;
+        // Parents with three of the same elements will have a 50 percent chance of sharing the same elemental mix,
+        if tile_elements.is_triple() {
+            // TODO refactor
+            let roll = rnjesus::d10();
+            if roll <= 5 {
+                elements = tile_elements.elements;
+            // a 30 percent chance of one of the three being randomly re rolled,
+            } else if roll <= 8 {
+                elements = reroll_one(tile_elements.elements);
+            // and a 20 percent chance for two to be re rolled.
+            } else {
+                elements = reroll_two(tile_elements.elements);
+            }
+            TileElements::new(elements)
+
+        // Those with two of the same have 80/15/5 percent chance
+        } else if tile_elements.is_double() {
+            let roll = rnjesus::d20();
+            if roll <= 16 {
+                elements = tile_elements.elements;
+                // a 30 percent chance of one of the three being randomly re rolled,
+            } else if roll <= 19 {
+                elements = reroll_one(tile_elements.elements);
+            // and a 20 percent chance for two to be re rolled.
+            } else {
+                elements = reroll_two(tile_elements.elements);
+            }
+            TileElements::new(elements)
+
+        // Those with all different have 90/5/5
+        } else {
+            let roll = rnjesus::d20();
+            if roll <= 18 {
+                elements = tile_elements.elements;
+                // a 30 percent chance of one of the three being randomly re rolled,
+            } else if roll <= 19 {
+                elements = reroll_one(tile_elements.elements);
+            // and a 20 percent chance for two to be re rolled.
+            } else {
+                elements = reroll_two(tile_elements.elements);
+            }
+            TileElements::new(elements)
+        }
+        // These are all just example numbers -
+        // and I haven't included a chance for three re-rolls, but there should probably be one too!
     }
+
+    fn is_triple(&self) -> bool {
+        [Element::Air, Element::Earth, Element::Fire, Element::Water]
+            .iter()
+            .any(|element| self.elements.iter().filter(|&n| *n == *element).count() == 3)
+    }
+
+    fn is_double(&self) -> bool {
+        [Element::Air, Element::Earth, Element::Fire, Element::Water]
+            .iter()
+            .any(|element| self.elements.iter().filter(|&n| *n == *element).count() == 2)
+    }
+}
+
+fn reroll_two(elements: ThreeElements) -> ThreeElements {
+    let which = rnjesus::rand_u8(0, 2);
+    // TODO lazy
+    let roll1 = rnjesus::rand_element();
+    let roll2 = rnjesus::rand_element();
+    let roll3 = rnjesus::rand_element();
+    [
+        if which == 0 { elements[0] } else { roll1 },
+        if which == 1 { elements[1] } else { roll2 },
+        if which == 2 { elements[2] } else { roll3 },
+    ]
+}
+
+fn reroll_one(elements: ThreeElements) -> ThreeElements {
+    let which = rnjesus::rand_u8(0, 2);
+    let roll = rnjesus::rand_element();
+    [
+        if which == 0 { roll } else { elements[0] },
+        if which == 1 { roll } else { elements[1] },
+        if which == 2 { roll } else { elements[2] },
+    ]
 }
 
 #[cfg(test)]
